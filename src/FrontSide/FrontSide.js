0ee3a5523402936641d4c83c24645e71c79e3415
Maksim <maksim.might@yandex.ru>
2018-08-26 19:45:39 +0300

    added  moment.js, icons and css files, also edited FrontSide component

--- src/FrontSide/FrontSide.js
+++ src/FrontSide/FrontSide.js
@@ -1,3 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './card.css';
+import optionsImage from './options.svg';
 
-export default () => <h1>FrontSide</h1>;
+export default class FrontSide extends Component {
+  render() {
+    return (
+      <div className="card is-clear-day">
+        <div className="card-row">
+          <div>Sunday</div>
+          <div>Aug 28</div>
+        </div>
+        <img src='/icons/clear-day.svg' alt='clear-day' />
+        <div className="card-row">
+          <div className="card-temperature">19
+            <span className="small">15</span>
+          </div>
+          <div className="card-weather">Sunny</div>
+        </div>
+        <div className="card-line" />
+        <div className="card-row">
+          <div className="card-city">Minsk</div>
+          <button>
+            <img src={optionsImage} width={32} alt='options' />
+          </button>
+        </div>
+      </div>
+    );
+  }
+};
